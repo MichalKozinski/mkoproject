42e7ebb87a6126b7a5bff1aa23dfa61bd2746b33
MichalKozinski <knf@vp.pl>
2024-01-22 21:35:50 +0100

    initializing django apps

---
+++ mkoproject/working_time_registration/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WorkingTimeRegistrationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'working_time_registration'
