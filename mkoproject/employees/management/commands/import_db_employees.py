117e29710ccc86027fbb5a18984686e5f18a232d
MichalKozinski <knf@vp.pl>
2024-01-25 14:12:14 +0100

    first try to make generator for data from workplaces. I'm going to take data who should have make its own db for production activity from db 'employees'

--- mkoproject/employees/management/commands/import_db_employees.py
+++ mkoproject/employees/management/commands/import_db_employees.py
@@ -30,3 +30,6 @@ class Command(BaseCommand):
                 if row['DOB']:
                     row['DOB']=fix_date(row['DOB'])
                 Employees.objects.create(EmpID=row['EmpID'], FirstName=row['FirstName'], LastName=row['LastName'], StartDate=row['StartDate'], ExitDate=row['ExitDate'], Title=row['Title'], Supervisor=row['Supervisor'], ADEmail=row['ADEmail'], EmployeeStatus=row['EmployeeStatus'], EmployeeType=row['EmployeeType'], PayZone=row['PayZone'], EmployeeClassificationType=row['EmployeeClassificationType'], TerminationType=row['TerminationType'], TerminationDescription=row['TerminationDescription'], DepartmentType=row['DepartmentType'], Division=row['Division'], DOB=row['DOB'], State=row['State'], JobFunctionDescription=row['JobFunctionDescription'], GenderCode=row['GenderCode'], LocationCode=row['LocationCode'], RaceDesc=row['RaceDesc'], MaritalDesc=row['MaritalDesc'], PerformanceScore=row['PerformanceScore'], CurrentEmployeeRating=row['CurrentEmployeeRating'])
+
+
+# to run this script: python manage.py import_db_employees
